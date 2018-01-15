d7e071dbbbc22c26e7a5bb5663e2578bf08e08b3
Tetiana Khotiaintseva <tetiana.khotiaintsev@allegro.pl>
2018-01-15 15:22:44 +0100

    Add card image on click

--- crystalsfreshfromkazakus/crystalsfreshfromkazakus.js
+++ crystalsfreshfromkazakus/crystalsfreshfromkazakus.js
@@ -1,6 +1,12 @@
+var display = document.getElementById("display");
 
-function displayCoords (event) {
-  console.log('X: ' + event.clientX + ', Y ' + event.clientY);
+function addImage (event) {
+  var image = document.createElement("div");
+  image.classList.add('image');
+  image.style.left = event.clientX - 143 + 'px';
+  image.style.top = event.clientY - 198 + 'px';
+
+  display.appendChild(image);
 }
 
-document.addEventListener('click', displayCoords)
+document.addEventListener('click', addImage)
