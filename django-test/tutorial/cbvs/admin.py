75c5b477d30a886f307dba8a40e8ebe0a7c0a913
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2018-01-20 22:49:10 +0100

    CRUD views

---
+++ django-test/tutorial/cbvs/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from cbvs.models import School, Student
+
+
+admin.site.register(School)
+admin.site.register(Student)
---
+++ django-test/tutorial/cbvs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CbvsConfig(AppConfig):
+    name = 'cbvs'
